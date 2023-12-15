1c7ac69002d917e69b4b1483ef57893c14821f8e
Mathius Mendoza <mendozamathius9@gmail.com>
2023-12-14 23:03:03 -0500

    ActualizadoProblemas

--- EjerciciosT9/src/paquete02/Ejercicio05.java
+++ EjerciciosT9/src/paquete02/Ejercicio05.java
@@ -21,7 +21,8 @@ public class Ejercicio05 {
         Antes de analizar el presente ejercicio,
         usted debe revisar y entender lo desarrollado
         en el archivo Ayuda01.java del paquete2
-        */
+         */
+
         Scanner entrada = new Scanner(System.in);
         String[] estudiantes = {"Kimberly", "Hogan",
             "Teresa", "Luis", "Mark", "Jennifer",
@@ -31,8 +32,23 @@ public class Ejercicio05 {
         boolean bandera = true;
         while (bandera) {
             System.out.println("Ingrese una letra");
-            inicial = entrada.nextLine();
-            
+            inicial = entrada.nextLine().toUpperCase();
+            // utilizamos un swich para selecionar las iniciales de los nombres, 
+            //de esa manera salir del ciclo de no serlo , lo pedira hasta que
+            //salga del ciclo y bandera se haga falso 
+            switch (inicial) {
+                case ("K"):
+                case ("H"):
+                case ("T"):
+                case ("L"):
+                case ("M"):
+                case ("J"):
+                case ("A"):
+                    bandera = false;
+                    break;
+                default:
+            }
+
         }
 
     }
